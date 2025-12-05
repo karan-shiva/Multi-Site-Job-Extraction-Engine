26cf4b6f39f35262c4eb8eadb7deed7eabec57b2
“karan65656” <karan65656@gmail.com>
2025-12-04 21:38:13 -0800

    Adding emojis in def.py

--- defs.py
+++ defs.py
@@ -1,3 +1,8 @@
-MIN_QUAL = "Minimum Qualifications"
-PREF_QUAL = "Prefered Qualifications"
+MIN_QUAL = "📋 Minimum Qualifications"
+PREF_QUAL = "🌟 Preferred Qualifications"
 OTHER_QUAL = "Other Requirements"
+
+
+"https://app.eightfold.ai/careers?query=Software%20Engineer&location=United%20States&pid=446704948282&domain=qualcomm.com&sort_by=relevance&triggerGoButton=false"
+
+"https://app.eightfold.ai/careers?query=Software%20Engineer&location=United%20States&pid=446704948277&domain=qualcomm.com&sort_by=relevance&triggerGoButton=false"
